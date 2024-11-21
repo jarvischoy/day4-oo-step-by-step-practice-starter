fbaaa3acf2d56f6612a7360c16957499c332c6e6
Jarvis Choy <choyja2@oocl.com>
2024-11-21 14:13:14 +0800

    feat: step 3

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -1,4 +1,28 @@
 package oo;
 
+import java.util.Objects;
+
 public class Klass {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return number == klass.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(number);
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -1,13 +1,22 @@
 package oo;
 
 public class Student extends Person {
+    private Klass klass;
 
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
 
+    public void join(Klass klass) {
+        this.klass = klass;
+    }
+
+    public boolean isIn(Klass klass) {
+        return klass.equals(this.klass);
+    }
+
     @Override
     public String introduce() {
-        return super.introduce().concat(" I am a student.");
+        return super.introduce().concat(String.format(" I am a student. I am in class %d.", klass.getNumber()));
     }
 }
